34ff05f20050fff78b28db494372033cad7b1c20
PatchyPhoenix <91737398+PatchyPhoenix@users.noreply.github.com>
2023-06-22 21:43:59 +0530

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,19 @@
 console.log("Hello world");
+
+var menuButton = document.getElementById("menuButton");
+
+let clicked = false;
+
+menuButton.addEventListener('click',function handleClick())
+{
+    if(clicked)
+    {
+        console.log("Closed");
+        click = false;
+    }
+    else
+    {
+        console.log("Opened");
+        click = true;
+    }
+}
